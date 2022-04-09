34091389b66153c27abc3c42e66d024985076986
sufwanks <sufwansaifu@gmail.com>
2022-04-09 18:26:27 +0530

    event binding

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  aim="Your perfect banking partner";
+  accnum="Enter your account number";
+  acno="";
+  pswd="";
+
+
+  dataBase:any={
+    1000:{acno:1000,uname:"kabil",password:1000,balance:5000},
+    1001:{acno:1001,uname:"neer",password:1001,balance:6000},
+    1002:{acno:1002,uname:"neelam",password:1002,balance:8000},
+    1003:{acno:1003,uname:"Moin",password:1003,balance:4000}
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  accountChange(event:any){
+    this.acno=event.target.value;    
+  }
+  pswdChange(event:any){
+    this.pswd=event.target.value;    
+  }
+
+  login(){
+
+    var acno= this.acno;
+    var pswd=this.pswd;
+
+    let dataBase=this.dataBase;
+     
+    if(acno in dataBase){
+      if(pswd== dataBase[acno]["password"]){
+        alert("access granted")
+      }
+      else{
+        alert("invalid password")
+      }
+    }
+    else{
+      alert("invalid account number")
+    }
+
+  }
+
+
+}
